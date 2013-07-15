12af68413e496a16bd51c038e2d10d688f116813
Pedro Maciel <pmbmaciel@gmail.com>
2013-07-15 12:19:03 +0200

    removed code related to matrix storage indexing factory

--- lss/cf3/lss/lss_index.cpp
+++ lss/cf3/lss/lss_index.cpp
@@ -6,7 +6,7 @@
 
 
 #include "lss_index.hpp"
-#include <cstdarg>  // NOTE: below the above because it's a "very ugly" header
+#include <cstdarg>  // NOTE: below the above because it's a "really ugly" header
 
 
 namespace cf3 {
@@ -14,16 +14,21 @@ namespace lss {
 namespace lss_index {
 
 
-// NOTE: I opted for the constructor registering multiple methods, thus using
-// variable arguments, because source files can only contain definitions (no
-// declarations) so I can't call member methods, overload operators (), << or >>
-// to register all entries into the factory instance outside of run-time.
+/*
+ * NOTE: I opted for the constructor registering multiple methods (using
+ * variable arguments) because source files can only contain definitions, and no
+ * declarations, so I can't call member methods, overload operators (), << or >>
+ * to register all entries into the factory at once outside of run-time.
+ *
+ * The first idea was to have two factories (matrix addressing and storage
+ * indexing) but the latter is maybe "too internal" so actually this could be
+ * simplified I guess.
+ */
 
-/// constructor registering built-in indexing techniques
+
 index_factory_t::index_factory_t(const size_t n, index_creator_t* c, ...) {
   if (n==1 && c!=NULL) Register(c);
   if (n<2) return;
-
   va_list ap;
   va_start(ap,c);
   for (size_t i=1; i<n; ++i)
@@ -32,20 +37,10 @@ index_factory_t::index_factory_t(const size_t n, index_creator_t* c, ...) {
 }
 
 
-#if 0
-// NOTE: maybe this should be addressed directly because it's internal storage and we don't want users to mess around these things?
-// register built-in matrix storage indexing techniques
-index_factory_t idx_factory_matrix_storage(
-    1,
-    new index_creator< idx_sparse_matrix_csr >("sparse_matrix_csr_indexing")
-    );
-#endif
-
-
-// register built-in linear system matrix addressing indexing techniques
+// register built-in linear system matrix addressing indexing(s)
 index_factory_t idx_factory_matrix_indexing(
     1,
-    new index_creator< idx_regular_block >("regular_block_indexing")
+    new index_creator< idx_regular_block >("regular_block")
     );
 
 
