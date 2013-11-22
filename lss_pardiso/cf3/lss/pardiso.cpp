e30f41e06a850a461700530295b499201e90760c
Pedro Maciel <pmbmaciel@gmail.com>
2013-11-22 21:58:29 +0100

    works, looks better and is even simpler now (win-win-win).

--- lss_pardiso/cf3/lss/pardiso.cpp
+++ lss_pardiso/cf3/lss/pardiso.cpp
@@ -37,22 +37,21 @@ pardiso::pardiso(const std::string& name, const size_t& _size_i, const size_t& _
          << "pardiso: PARDISO_LIC_PATH: " << licspath .description() << CFendl
          << "pardiso: MKL_SERIAL:       " << mklserial.description() << " (should be set to YES)" << CFendl;
 
-  mtype = 1; // real and structurally symmetric matrix
+  mtype  = 1;  // real and structurally symmetric matrix
+  maxfct = 1;  // maximum number of numerical factorizations
+  mnum   = 1;  // which factorization to use
+
+  for (size_t i=0; i<64; ++i) iparm[i] = 0;
+  for (size_t i=0; i<64; ++i) dparm[i] = 0.;
 
   // reset pt, iparm and dparm defaults
-  std::fill_n(&iparm[0],64,0);
-  std::fill_n(&dparm[0],64,0.);
   int err = 0;
   pardisoinit_(pt,&mtype,&iparm[31],iparm,dparm,&err);
   if (err)
     throw std::runtime_error(err_message(err));
-
-  iparm[ 2] = nthreads.value;
-  iparm[ 7] = 0;  // max numbers of iterative refinement steps
-  iparm[31] = 0;  // [0|1] sparse direct solver or multi-recursive iterative solver
-  maxfct    = 1;  // maximum number of numerical factorizations
-  mnum      = 1;  // which factorization to use
-  msglvl    = 1;  // message level: output statistical information
+  iparm[ 2] = nthreads.value;  // + set nb. threads
+  iparm[ 7] = 0;               // + max numbers of iterative refinement steps
+  iparm[31] = 0;               // + [0|1] sparse direct solver or multi-recursive iterative solver
 
   linearsystem< double >::initialize(_size_i,_size_j,_size_k,_value);
 }
@@ -60,23 +59,17 @@ pardiso::pardiso(const std::string& name, const size_t& _size_i, const size_t& _
 
 pardiso::~pardiso()
 {
-  call_pardiso(-1);  // -1: termination and release of memory
+  call_pardiso(-1,0);  // -1: termination and release of memory
 }
 
 
 pardiso& pardiso::solve()
 {
-  nrhs = static_cast< int >(m_b.size(1));
-
   int err;
-#if 0
-  if ((err=call_pardiso_printstats()) ||  // check for matrix/vector consistency
-      (err=call_pardiso(11))          ||  // 11: reordering and symbolic factorization
-      (err=call_pardiso(22))          ||  // 22: numerical factorization and
-      (err=call_pardiso(33)))             // 33: back substitution and iterative refinement
-#else
-  if (err=call_pardiso(13))
-#endif
+  if ( (err=call_pardiso_printstats()) ||  // check for matrix/vector consistency
+       (err=call_pardiso(11,0))        ||  // 11: reordering and symbolic factorization
+       (err=call_pardiso(22,0))        ||  // 22: numerical factorization and
+       (err=call_pardiso(33,0)) )          // 33: back substitution and iterative refinement
     throw std::runtime_error(err_message(err));
   return *this;
 }
@@ -89,10 +82,8 @@ pardiso& pardiso::copy(const pardiso& _other)
   std::copy(&_other.pt   [0],&_other.pt   [0]+64,&pt   [0]);
   std::copy(&_other.dparm[0],&_other.dparm[0]+64,&dparm[0]);
   std::copy(&_other.iparm[0],&_other.iparm[0]+64,&iparm[0]);
-  nrhs   = _other.nrhs;
   maxfct = _other.maxfct;
   mnum   = _other.mnum;
-  msglvl = _other.msglvl;
   mtype  = _other.mtype;
   return *this;
 }
@@ -124,15 +115,16 @@ std::string pardiso::err_message(const int& err)
 }
 
 
-int pardiso::call_pardiso(const int& _phase)
+int pardiso::call_pardiso(int _phase, int _msglvl)
 {
   matrix_t::matrix_compressed_t& A = m_A.compress();
-  int phase = _phase;
+  int nrhs = static_cast< int >(m_b.size(1));
+
   int err = 0;
   pardiso_(
-    pt, &maxfct, &mnum, &mtype, &phase,
+    pt, &maxfct, &mnum, &mtype, &_phase,
     &A.nnu, &A.a[0], &A.ia[0], &A.ja[0],
-    NULL, &nrhs, iparm, &msglvl, &m_b.a[0], &m_x.a[0], &err, dparm );
+    NULL, &nrhs, iparm, &_msglvl, &m_b.a[0], &m_x.a[0], &err, dparm );
   return err;
 }
 
@@ -140,6 +132,8 @@ int pardiso::call_pardiso(const int& _phase)
 int pardiso::call_pardiso_printstats()
 {
   matrix_t::matrix_compressed_t& A = m_A.compress();
+  int nrhs = static_cast< int >(m_b.size(1));
+
   int err = 0;
   pardiso_printstats_(
     &mtype,
--- lss_pardiso/cf3/lss/pardiso.hpp
+++ lss_pardiso/cf3/lss/pardiso.hpp
@@ -56,7 +56,7 @@ class lss_API pardiso : public linearsystem< double >
   static std::string err_message(const int& err);
 
   /// Library call
-  int call_pardiso(const int& _phase);
+  int call_pardiso(int _phase, int _msglvl);
 
   /// Library call (print statistics)
   int call_pardiso_printstats();
@@ -71,8 +71,9 @@ class lss_API pardiso : public linearsystem< double >
   void A___initialize(const size_t& i, const size_t& j, const double& _value=double()) { m_A.initialize(i,j,_value); }
   void A___initialize(const std::vector< double >& _vector) { m_A.initialize(_vector); }
   void A___initialize(const std::string& _fname)            { m_A.initialize(_fname);  }
-  void A___clear()                  { m_A.clear();    }
-  void A___zerorow(const size_t& i) { m_A.zerorow(i); }
+  void A___assign(const double& _value) { m_A = _value;   }
+  void A___clear()                      { m_A.clear();    }
+  void A___zerorow(const size_t& i)     { m_A.zerorow(i); }
   void A___sumrows(const size_t& i, const size_t& isrc) { m_A.sumrows(i,isrc); }
 
   void   A___print(std::ostream& o, const print_t& l=print_auto) const { m_A.print(o,l); }
@@ -85,10 +86,8 @@ class lss_API pardiso : public linearsystem< double >
   void*  pt[64];  // internal memory pointer (void* for both 32/64-bit)
   double dparm[64];
   int    iparm[64],
-         nrhs,
          maxfct,
          mnum,
-         msglvl,
          mtype;
 
 };
