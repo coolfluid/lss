11f500d748836cb605462655007949f777aaae2b
Pedro Maciel <pmbmaciel@gmail.com>
2014-02-25 20:56:47 +0100

    [DEV] specialized complex linearsystem base class builders (for abstract components).

--- lss/cf3/lss/linearsystem.cpp
+++ lss/cf3/lss/linearsystem.cpp
@@ -15,8 +15,10 @@ namespace cf3 {
 namespace lss {
 
 
-common::RegisterComponent< linearsystem< double >, LibLSS > Register_linearsystem;
-common::RegisterComponent< linearsystem< float  >, LibLSS > Register_linearsystem_SinglePrecision;
+common::RegisterComponent< linearsystem< double  >, LibLSS > Register_linearsystem_LongPrecisionReal;
+common::RegisterComponent< linearsystem< zdouble >, LibLSS > Register_linearsystem_LongPrecisionComplex;
+common::RegisterComponent< linearsystem< float   >, LibLSS > Register_linearsystem_ShortPrecisionReal;
+common::RegisterComponent< linearsystem< zfloat  >, LibLSS > Register_linearsystem_ShortPrecisionComplex;
 
 
 }  // namespace lss
